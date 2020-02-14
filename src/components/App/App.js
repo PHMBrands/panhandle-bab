a7c9ca1271fa29399723d0f1f0bfe7e90c18ba51
Mark R. Collins <markcollins.md@gmail.com>
2020-02-14 12:38:39 -0800

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../images/logo.svg';
+import ProgressiveForm from '../ProgressiveForm/ProgressiveForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>Panhandle Milling</p>
+        <p>Build a Brand</p>
+      </header>
+      <ProgressiveForm />
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ProgressiveForm/ProgressiveForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ProgressiveForm extends Component {
+  constructor() {
+    super()
+
+    this.state = {}
+  }
+
+  render() {
+    
+    return (
+      <div>Test component</div>
+    )
+  }
+}
+
+export default ProgressiveForm;
